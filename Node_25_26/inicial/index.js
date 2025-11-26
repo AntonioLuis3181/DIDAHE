283192f7c1cf0034f73ef9ac1021275df2be4fbf
AntonioLuis3181 <xavevel449@ieshnosmachado.org>
2025-11-26 14:44:29 +0100

    Node

--- Node_25_26/inicial/index.js
+++ Node_25_26/inicial/index.js
@@ -1,5 +1,31 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// Definimos el middleware
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+// Preparamos express para que admita datos de entrada JSON
+app.use(express.json());
+app.use(requestLogger);
+
+// Admitimos todos los origenes
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:8081"], // Permite el fronted el desarrollo React y React Native
+    credentials: true, //Permitir envio de cookies
+  })
+);
+
+app.use(express.static("public"));
 
 let notes = [
   {
@@ -19,7 +45,7 @@ let notes = [
   },
 ];
 
-app.get("/", (request, response) => {
+app.get("/", requestLogger, (request, response) => {
   response.send("<h1>Hola Mundo cruel!</h1>");
 });
 
@@ -30,8 +56,54 @@ app.get("/api/notes", (request, response) => {
 app.get("/api/notes/:id", (request, response) => {
   const id = parseInt(request.params.id);
   const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response
+      .status(404)
+      .json({ mensaje: "No existe la nota con el id:" + id })
+      .end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  // Si no llega el atributo content ==> ERROR 400
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false, // Si no llega importan ==> valor false
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
