52e91f76471220c84c5b01e8b002b84bcfd2192c
AntonioLuis3181 <xavevel449@ieshnosmachado.org>
2026-01-12 09:36:13 +0100

    cambios

--- Odoo/addons/library/models/editorial.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class editorial(models.Model):
-    _name = "library.editorial"
-
-    name = fields.Char("Editorial",size=64, required=True)
-    book_ids= fields.One2many("library.book", "editorial_id","Libros")
